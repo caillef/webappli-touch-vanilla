2dbb9d203560539976f2858947bdc52bf7f2c85f
Corentin CAILLEAUD <corentin.cailleaud@epitech.eu>
2021-01-10 22:55:31 +0100

    Update

--- js/game.js
+++ js/game.js
@@ -163,7 +163,7 @@ function motion(event){
     ctx.fill();
     ctx.closePath();
 
-    z += event.accelerationIncludingGravity.z
+    z = event.accelerationIncludingGravity.z
     y += event.accelerationIncludingGravity.x
     x += event.accelerationIncludingGravity.y
   }
